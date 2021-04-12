5995ac6b96428aba468036239985f91d35a88f08
jkr174 <romo17410@gmail.com>
2021-04-12 17:09:59 -0500

    Application Design

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace KWSalesOrderFormProject
+{
+    partial class frmRented
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.PrintDocument = new System.Drawing.Printing.PrintDocument();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.Filter = "SQL Server Databases (*.mdf)|*.mdf";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(434, 524);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "&Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(497, 334);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(353, 524);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "&Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 524);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmRented
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 559);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmRented";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rent Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmKWSales_FormClosing);
+            this.Load += new System.EventHandler(this.frmKWSales_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Drawing.Printing.PrintDocument PrintDocument;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KWSalesOrderFormProject
+{
+    public partial class frmRented : Form
+    {
+        bool selectedFile = true;
+
+        SqlConnection toolRentalConnection;
+        SqlCommand rentalCommand;
+        SqlDataAdapter rentalAdapter;
+        DataTable rentalTable;
+        public frmRented()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // connect to sales database
+                    toolRentalConnection = new SqlConnection("Data Source =.\\SQLEXPRESS;" +
+                        "AttachDbFilename=" + dlgOpen.FileName + "; " +
+                        "Integrated Security=True;" +
+                        "Connect Timeout=30;" +
+                        "User Instance=True");
+                    toolRentalConnection.Open();
+                    
+                }
+                catch (Exception ex)
+                {
+                    selectedFile = false;
+                    MessageBox.Show(
+                        ex.Message,
+                        "Error establishing database file connection.",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                selectedFile = false;
+                MessageBox.Show(
+                    "No file selected. Program will now exit.",
+                    "Program stopping",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                this.Close();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmKWSales_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void frmKWSales_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            if (selectedFile != false)
+            {
+                toolRentalConnection.Close();
+                rentalCommand.Dispose();
+                rentalAdapter.Dispose();
+                rentalTable.Dispose();
+            }
+        }
+    }
+}
